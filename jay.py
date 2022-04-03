8d14b836f69bf49045c4dfb6f0cfa8a588efd7b6
Jayprakashpandit12345 <jayprakashpandit2001@gmail.com>
2022-04-03 12:50:45 +0530

    2nd commit

--- jay.py
+++ jay.py
@@ -1,3 +1,14 @@
 print("jay")
 print("jay2")
 print("")
+"""
+git --version
+git status
+git config --global user.email ""
+git config --global user.name ""
+git remote add origin 
+git init 
+git add
+git commit -m ""
+git push -u origin master
+"""
